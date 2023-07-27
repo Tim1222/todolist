5ce746476a9b8d96cbc6d630fdb2dfe66d5314d7
Tim1222 <timofeu47@gmail.com>
2023-07-27 23:02:25 +0300

    Updates

---
+++ precache-manifest.bcfdd77cafa90435de90db0b693304da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bd830546e089d2464b6",
+    "url": "/todolist/static/css/main.7e06eef9.chunk.css"
+  },
+  {
+    "revision": "4bd830546e089d2464b6",
+    "url": "/todolist/static/js/main.4bd83054.chunk.js"
+  },
+  {
+    "revision": "21567bc97504e9c17cab",
+    "url": "/todolist/static/js/1.21567bc9.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "5eff936648dd2f56f335c3fe264b54fb",
+    "url": "/todolist/index.html"
+  }
+];
